91700a61943f5572edfce5c58f0b31c88390745a
Антон <Anton@MacBook-Pro-Anton-6.local>
2020-07-02 23:54:27 +0300

    Личный проект: модуляция (часть 2)

--- js/photo-editing.js
+++ js/photo-editing.js
@@ -3,34 +3,96 @@
 (function () {
   // получение уровня эффекта
   var pinEffectLevel = document.querySelector('.effect-level__pin');
+  var depthEffectLevel = document.querySelector('.effect-level__depth');
   var radioEffect = document.getElementsByName('effect');
+  var effectLevelValue = document.querySelector('.effect-level__value');
+  var levelEffect = document.querySelector('.effect-level');
   var textDescription = document.querySelector('.text__description');
   var effectLevelProportion = 0;
-  var effect = 'none';
-
-  var effectLevelValue = document.querySelector('.effect-level__value');
+  var nameEffect = 'none';
+  var arrayEffects = {
+    'none': ['', '', ''],
+    'chrome': ['grayscale', '1', ''],
+    'sepia': ['sepia', '1', ''],
+    'marvin': ['invert', '100', '%'],
+    'phobos': ['blur', '10', 'px'],
+    'heat': ['brightness', '3', '']
+  };
 
   window.main.arrayImportantElements.push(textDescription);
 
+  levelEffect.classList.add('hidden');
+
+  var applicationEffect = function (Effect, effectLevel) {
+    var img = document.querySelector('.img-upload__preview');
+    img.style.filter = arrayEffects[Effect][0] + '(' + arrayEffects[Effect][1] * effectLevel + arrayEffects[Effect][2] + ')';
+  };
+
   // получение пропорции уровня эффекта и запись ее в input
-  var gettingValueEffect = function (evt) {
-    effectLevelProportion = evt.target.offsetLeft / evt.target.parentNode.offsetWidth;
+  var gettingValueEffect = function (pin) {
+    effectLevelProportion = (pin.offsetLeft / pin.parentNode.offsetWidth) * 100;
+    effectLevelValue.value = Math.round(effectLevelProportion);
+    return Math.round(effectLevelProportion) / 100;
   };
 
   // обнуление эффекта при переключении и получение его названия
   var gettingValueRadioEffect = function (evt) {
     effectLevelProportion = 0;
     effectLevelValue.value = 0;
-    effect = evt.target.value;
+    pinEffectLevel.style.left = 0;
+    depthEffectLevel.style.width = 0;
+
+    if (evt.target.value === 'none') {
+      levelEffect.classList.add('hidden');
+      pinEffectLevel.removeEventListener('mousedown', onPinMove);
+    } else {
+      levelEffect.classList.remove('hidden');
+      pinEffectLevel.addEventListener('mousedown', onPinMove);
+      applicationEffect(evt.target.value, 0);
+      nameEffect = evt.target.value;
+    }
   };
 
-  pinEffectLevel.addEventListener('mouseup', gettingValueEffect);
+  var onPinMove = function (evt) {
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMauseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      if ((pinEffectLevel.offsetLeft - shift.x) >= 0 && pinEffectLevel.parentNode.offsetWidth >= (pinEffectLevel.offsetLeft - shift.x)) {
+        pinEffectLevel.style.left = (pinEffectLevel.offsetLeft - shift.x) + 'px';
+        depthEffectLevel.style.width = (depthEffectLevel.offsetWidth - shift.x) + 'px';
+        applicationEffect(nameEffect, gettingValueEffect(pinEffectLevel));
+      }
+    };
+
+    var onMauseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMauseMove);
+      document.removeEventListener('mouseup', onMauseUp);
+    };
+
+    document.addEventListener('mousemove', onMauseMove);
+    document.addEventListener('mouseup', onMauseUp);
+  };
 
   for (var i = 0; i < radioEffect.length; i++) {
     radioEffect[i].addEventListener('change', gettingValueRadioEffect);
   }
-
-  effectLevelValue.value = Math.round(effectLevelProportion * 100) + ' ' + effect;
 
   var hashtag = document.querySelector('.text__hashtags');
   // добавляет элемент в массив для отслеживания
