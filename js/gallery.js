8d0beafc1ee786f0efd404d89cb92784324dd8d5
Антон <Anton@MacBook-Pro-Anton-6.local>
2020-07-20 22:20:24 +0300

    Личный проект: перламутровые пуговицы (часть 1)

--- js/gallery.js
+++ js/gallery.js
@@ -151,7 +151,6 @@
   document.querySelector('.social__comment-count').classList.add('hidden');
   document.querySelector('.comments-loader').classList.add('hidden');
 
-
   window.gallery = {
     addPhoto: addPhoto,
     fillingBigPicture: fillingBigPicture,
