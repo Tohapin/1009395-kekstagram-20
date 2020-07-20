1f80246d7ff8750d16bd3d61974cf4242d3e9ca7
Антон <Anton@MacBook-Pro-Anton-6.local>
2020-07-20 18:45:32 +0300

    Личный проект: перламутровые пуговицы (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
 'use strict';
 
 (function () {
-  // массив, в который можно поместить элементы для отслеживания, чтобы окно не закрывалось при активном состоянии элементов
-  var arrayImportantElements = [];
+  var DEBOUNCE_INTERVAL = 500;
+  var arrayImportantElements = []; // массив, в который можно поместить элементы для отслеживания, чтобы окно не закрывалось при активном состоянии элементов
 
-  var randomInteger = function (min, max) {
+  var randomInteger = function (min, max, exclusion) {
     var integer = Math.round(Math.random() * 10);
 
-    while (integer >= max || min >= integer) {
-      integer = Math.round(Math.random() * 100);
+    if (exclusion) {
+      while ((integer >= max || min >= integer) || exclusion.includes(integer)) {
+        integer = Math.round(Math.random() * 100);
+      }
+    } else {
+      while (integer >= max || min >= integer) {
+        integer = Math.round(Math.random() * 100);
+      }
     }
-
     return integer;
   };
+
+  var debounce = function (cb) {
+    var lastTimeout = null;
+
+     return function() {
+      var parameters = arguments;
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(function() {
+        cb.apply(null, parameters);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
+
 
   var closePopup = function (popup, btnClose, form) {
     var onPopupEscPress = function (evt) {
@@ -66,6 +86,7 @@
   window.main = {
     randomInteger: randomInteger,
     arrayImportantElements: arrayImportantElements,
-    closePopup: closePopup
+    closePopup: closePopup,
+    debounce: debounce
   };
 })();
